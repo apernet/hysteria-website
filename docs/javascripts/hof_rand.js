64eb5907afa3eb5e984904aee0af6f08d15ea886
Toby <tobyxdd@gmail.com>
2024-05-03 11:19:42 -0700

    make sure apernet stay on top

--- docs/javascripts/hof_rand.js
+++ docs/javascripts/hof_rand.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const list = heading.nextElementSibling;
         let items = Array.from(list.querySelectorAll('li'));
         for (let i = items.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
+            const j = Math.floor(Math.random() * (i)) + 1; // Apernet should always stay at the top
             [items[i], items[j]] = [items[j], items[i]];
         }
         items.forEach(item => list.appendChild(item));
